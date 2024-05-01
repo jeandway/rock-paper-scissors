acd8021ab13ab26f6d28d10aff7ba2e5bc4ee011
Jean Dilloway <119371599+jeandway@users.noreply.github.com>
2024-04-30 23:40:25 -0400

    main.py

--- main.py
+++ main.py
@@ -30,23 +30,21 @@ scissors = '''
 choices = [rock, paper, scissors]
 
 userPlayer = int(input("what do you choose? type 0 for rock, 1 for paper, 2 for scissors\n"))
-print("You choose: ") 
-print(choices[userPlayer])
-
-computerPlayer = random.randint(0,2)
-print("Computer choose: ")
-print(choices[computerPlayer])
-
-
 if userPlayer >= 3 or userPlayer < 0: 
   print("You typed an invalid number, you lose!") 
-elif userPlayer == 0 and computerPlayer == 2:
-  print("You win!")
-elif computerPlayer == 0 and userPlayer == 2:
-  print("You lose")
-elif computerPlayer > userPlayer:
-  print("You lose")
-elif userPlayer > computerPlayer:
-  print("You win!")
-elif computerPlayer == userPlayer:
-  print("It's a draw")
+else:
+  print("You choose: ")
+  print(choices[userPlayer])
+  computerPlayer = random.randint(0,2)
+  print("Computer choose: ")
+  print(choices[computerPlayer])
+  if userPlayer == 0 and computerPlayer == 2:
+    print("You win!")
+  elif computerPlayer == 0 and userPlayer == 2:
+    print("You lose")
+  elif computerPlayer > userPlayer:
+    print("You lose")
+  elif userPlayer > computerPlayer:
+    print("You win!")
+  elif computerPlayer == userPlayer:
+    print("It's a draw")
